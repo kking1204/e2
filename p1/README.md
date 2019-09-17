2853410934e16f031117a1970c4a80e32cecec8b
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-09-17 10:44:21 -0400

    finishing touches on P1

--- p1/README.md
+++ p1/README.md
@@ -1,9 +1,9 @@
 # Project 1: Workflow setup
-+ By: Kenyon King
++ By: Kenyon M. King
 + Production URL: p1.digitalcrimson.org
 
 # Notes for the instructor
 + Fun! 
-+ I used an image of my dog Oscar. (Old dogs...new trick...I think you get the idea!) I took the photo years ago, own the rights, yada yada yada
++ I used an image of my dog Oscar. (Old dogs...new trick...I think you get the idea!) I took the photo years ago, I own the rights.
 + Thank you for the extensive documentation and screenshots in [E-2 Notes] (https://hesweb.dev/e2/notes#/)
 
--- p1/index.php
+++ p1/index.php
@@ -8,14 +8,22 @@
 		width: 900px;
 		padding: 5em;
 		}	
+	p.strike {
+		text-decoration: line-through; 
+	}
+	img {
+		max-width: 600px;
+	}	
 </style>
 </head>
 
 <body>
 	<main>
 	<h1>Hello!</h1>
-	<p>I work at the Extension School. I want to get better at PHP.</p>
-	<img src="images/oscar.jpg" alt="old dog">
+		<p class="strike">My name is Inigo Montoya. You killed my father. Prepare to die.</p>
+		<p>My name is Kenyon M. King. I work at the Extension School. I want to get better at PHP.</p>
+		<img src="images/oscar.jpg" alt="old dog">
+		<p>This is Oscar. We both prefer Macintosh computers.</p>
 	</main>
 </body>
 </html>
