b39aa01bf3d88adc214139ef6d3139a627da8c66
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-09-29 14:12:46 -0400

    fleshing out the controller

--- p2/README.md
+++ p2/README.md
@@ -3,6 +3,7 @@
 + Production URL: <http://p2.digitalcrimson.org>
 
 ## Game planning
+
 On page load, I'll display what Player A threw, and what Player B threw.
 I'll display who the winner is based on our understanding of rock beating scissors, scissors beating paper, etc.
 
---
+++ p2/game-controller.php
@@ -0,0 +1,30 @@
+<?php
+// this is the brains of the rock paper scissors game
+
+
+
+$throw = ['rock', 'paper', 'scissors']; 
+
+$playerAMove = $throw[rand(0, 2)]; // pick a random string
+$playerBMove = $throw[rand(0, 2)]; // rock (0) paper (1) or scissors (2)
+
+var_dump($playerAMove);
+var_dump($playerBMove);
+
+if ($playerAMove == $playerBMove) {
+    var_dump("it's a tie");
+} elseif ($playerAMove == 'rock' and $playerBMove == 'paper') {
+    var_dump('Player B wins');
+} elseif ($playerAMove == 'rock' and $playerBMove == 'scissors') {
+    var_dump('Player A wins');
+} elseif ($playerAMove == 'paper' and $playerBMove == 'scissors') {
+    var_dump('Player B wins');
+} elseif ($playerAMove == 'paper' and $playerBMove == 'rock') {
+    var_dump('Player A wins');
+} elseif ($playerAMove == 'scissors' and $playerBMove == 'paper') {
+    var_dump('Player A wins');
+} elseif ($playerAMove == 'scissors' and $playerBMove == 'rock') {
+    var_dump('Player B wins');
+}
+
+
