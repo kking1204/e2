2e6afa70baba54b3e785b768d7676cf6a850e616
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-09-29 14:14:36 -0400

    commenting out my var_dumps

--- p2/game-controller.php
+++ p2/game-controller.php
@@ -8,23 +8,23 @@ $throw = ['rock', 'paper', 'scissors'];
 $playerAMove = $throw[rand(0, 2)]; // pick a random string
 $playerBMove = $throw[rand(0, 2)]; // rock (0) paper (1) or scissors (2)
 
-var_dump($playerAMove);
-var_dump($playerBMove);
+// var_dump($playerAMove);
+// var_dump($playerBMove);
 
 if ($playerAMove == $playerBMove) {
-    var_dump("it's a tie");
+    // var_dump("it's a tie");
 } elseif ($playerAMove == 'rock' and $playerBMove == 'paper') {
-    var_dump('Player B wins');
+    // var_dump('Player B wins');
 } elseif ($playerAMove == 'rock' and $playerBMove == 'scissors') {
-    var_dump('Player A wins');
+    // var_dump('Player A wins');
 } elseif ($playerAMove == 'paper' and $playerBMove == 'scissors') {
-    var_dump('Player B wins');
+    // var_dump('Player B wins');
 } elseif ($playerAMove == 'paper' and $playerBMove == 'rock') {
-    var_dump('Player A wins');
+    // var_dump('Player A wins');
 } elseif ($playerAMove == 'scissors' and $playerBMove == 'paper') {
-    var_dump('Player A wins');
+    // var_dump('Player A wins');
 } elseif ($playerAMove == 'scissors' and $playerBMove == 'rock') {
-    var_dump('Player B wins');
+    // var_dump('Player B wins');
 }
 
 
