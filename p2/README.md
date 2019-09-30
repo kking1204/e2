be4df7f0d3dde236a838866cf5e28690de4e792c
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-09-30 14:21:25 -0400

    some minimal CSS applied, updated README

--- p2/README.md
+++ p2/README.md
@@ -11,5 +11,8 @@
 * On index.php page load, I'll display what Player A threw, and what Player B threw, and who the winner is.
 
 ## Outside resources
-I used the lecture notes that linked to PHP.net
-I used the W3C HTML validator
+* I used the lecture notes that linked to PHP.net
+* I used the W3C HTML validator
+
+## Notes for instructor
+At the end, I spruced up the HTML page with some basic colors and styles. I added a sentece that reads "Refresh the page to play again!" - I'm aware that the project specified no user input required, but it doesn't involve any PHP programming on my part. I think it's kosher to leave it in? Thanks!
--- p2/index.php
+++ p2/index.php
@@ -4,12 +4,21 @@
 <head>
     <title>Project 2: Rock Paper Scissors</title>
     <meta charset='utf-8'>
+        <style>
+            body {background-color: powderblue; width: 950px; margin: 0 auto; font-family: sans-serif}
+            h1   {color: blue; text-decoration: overline}
+            strong {color: red; font-size: larger}
+        </style>
 </head>
+
 <body>
     <main>
-        <p>Player A threw <?php echo $playerAMove ?></p>
-        <p>Player B threw <?php echo $playerBMove ?></p>
-        <p>The winner is ... <?php echo $winner ?></p>
+    <h1>Let's play Rock Paper Scissors</h1>
+        <p>Player A threw <strong><?php echo $playerAMove ?></strong></p>
+        <p>Player B threw <strong><?php echo $playerBMove ?></strong></p>
+        <p>The winner is ... <strong><?php echo $winner ?></strong></p>
+        <br>
+        <p><em>Refresh the page to play again!</em></p>
     </main>
 </body>
 </html>
