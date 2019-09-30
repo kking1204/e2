3c8b99b24a8cc7fb4e6bf6b8566d02c902e30aae
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-09-30 11:31:52 -0400

    cleaning up extra whitespace and unnedded comments

--- p2/game-controller.php
+++ p2/game-controller.php
@@ -12,9 +12,6 @@ $throw = ['rock', 'paper', 'scissors'];
 $playerAMove = $throw[rand(0, 2)]; // pick a random string
 $playerBMove = $throw[rand(0, 2)]; // rock (0) paper (1) or scissors (2)
 
-// var_dump($playerAMove);
-// var_dump($playerBMove);
-
 if ($playerAMove == $playerBMove) {
     $winner = ("it's a tie");
 } elseif ($playerAMove == 'rock' and $playerBMove == 'paper') {
@@ -30,5 +27,3 @@ if ($playerAMove == $playerBMove) {
 } elseif ($playerAMove == 'scissors' and $playerBMove == 'rock') {
     $winner = ("Player B");
 }
-
-
