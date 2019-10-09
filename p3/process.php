d10bbc963714debda8c0b8c5bac53d5b2f3908c3
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-10-09 10:52:31 -0400

    prrof of concetp, operational logic working

--- p3/process.php
+++ p3/process.php
@@ -9,11 +9,21 @@ $throwOptions = ['rock', 'paper', 'scissors'];
 $computerMove = $throwOptions[rand(0, 2)]; // pick a radnom rock (0) paper (1) or scissors (2)
 var_dump($computerMove); //proof of concept
 
-
-if ($userMove == $computerMove) {
-    var_dump("it's a tie"); //proof of concept
     
-    //another way to store and display it 
-    $results = "TIE";
-    var_dump($results);
-    } 
+if ($userMove == $computerMove) {
+    $winner = ("it's a tie");
+} elseif ($userMove == 'rock' and $computerMove == 'paper') {
+    $winner = ("Player B");
+} elseif ($userMove == 'rock' and $computerMove == 'scissors') {
+    $winner = ("Player A");
+} elseif ($userMove == 'paper' and $computerMove == 'scissors') {
+    $winner = ("Player B");
+} elseif ($userMove == 'paper' and $computerMove == 'rock') {
+    $winner = ("Player A");
+} elseif ($userMove == 'scissors' and $computerMove == 'paper') {
+    $winner = ("Player A");
+} elseif ($userMove == 'scissors' and $computerMove == 'rock') {
+    $winner = ("Player B");
+};
+
+    var_dump($winner); //proof of concept
