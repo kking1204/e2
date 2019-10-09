cdd50a2666d2bf6cd92ca2edc7f22f2074c5bd05
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-10-09 16:51:45 -0400

    have a simple Design A working except for Null value scenario

--- p3/process-controller.php
+++ p3/process-controller.php
@@ -1,42 +1,25 @@
 <?php
 
-session_start(); //invoke the Session
-	 
 $userMove = ($_GET['throw']); //  create variable for user choice
-var_dump($userMove); //proof of concept
 
 $throwOptions = ['rock', 'paper', 'scissors']; 
 
 // 	create variable for computer choice (random)
 $computerMove = $throwOptions[rand(0, 2)]; // pick a radnom rock (0) paper (1) or scissors (2)
-var_dump($computerMove); //proof of concept
 
     
 if ($userMove == $computerMove) {
     $winner = ("it's a tie");
 } elseif ($userMove == 'rock' and $computerMove == 'paper') {
-    $winner = ("Player B");
+    $winner = ("You lose! Paper covers rock");
 } elseif ($userMove == 'rock' and $computerMove == 'scissors') {
-    $winner = ("Player A");
+    $winner = ("You win - rock beats scissors");
 } elseif ($userMove == 'paper' and $computerMove == 'scissors') {
-    $winner = ("Player B");
+    $winner = ("You lose! Scissors cut paper!");
 } elseif ($userMove == 'paper' and $computerMove == 'rock') {
-    $winner = ("Player A");
+    $winner = ("You win! Paper covers rock");
 } elseif ($userMove == 'scissors' and $computerMove == 'paper') {
-    $winner = ("Player A");
+    $winner = ("You win! scissors cut paper");
 } elseif ($userMove == 'scissors' and $computerMove == 'rock') {
-    $winner = ("Player B");
+    $winner = ("You win - rock beats scissors");
 };
-
-    var_dump($winner); //proof of concept
-    
- 
- 
- 
- $_SESSION['thro'] = $_GET['throw'];   
- 
- $d = $_SESSION['thro'];
- 
- var_dump($d);
- 
- header('Location: index.php')
--- p3/process.php
+++ p3/process.php
@@ -1,2 +1,18 @@
-//the display page
-
+<?php require 'process-controller.php';?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	    <meta charset="utf-8">
+	    <title>E2 P2 Design A</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="###.css" />
+        <style>
+            body {background-color: powderblue; width: 950px; margin: 0 auto; font-family: sans-serif}
+        </style>
+	</head>
+	<body>
+		<p> You chose <?php echo($userMove);?>
+		<p> The computah chose <?php echo($computerMove);?>
+		<p> <?php echo($winner);?>
+		<p> <a href="index.php">Play again</a> </p>
+	</body>
+</html>
