d4d3454453d5008e967b9b5b73973b95dbb0f2ca
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-10-22 10:29:14 -0400

    I must’ve accidentally commited changes to this file when I borrowed code chunks for P3. I’m simply reverting it to my working version that was already graded

--- p2/game-controller.php
+++ p2/game-controller.php
@@ -1,16 +1,13 @@
 <?php
 // this is the brains of the rock paper scissors game
 // * I define a variable ($throw) that contains 'rocks', 'paper' 'scissors'
-// * I then define two variables, PlayerAMove and PlayerBMove. 
+// * I then define two variables, PlayerAMove and PlayerBMove.
 // * PlayerAMove chooses a string at random from the $throw variable: 'rocks' (0), 'paper' (1) or 'scissors' (2)
 // * PlayerBMove chooses a string at random from the $throw variable: 'rocks' (0), 'paper' (1) or 'scissors' (2)
 // * Using conditional logic, I'll declare the winner based on our understanding of rock beating scissors, scissors beating paper, etc.
-
-
-$options = ['rock', 'paper', 'scissors']; 
-
-
-$computer = $options[rand(0, 2)]; // rock (0) paper (1) or scissors (2)
+$throw = ['rock', 'paper', 'scissors'];
+$playerAMove = $throw[rand(0, 2)]; // rock (0) paper (1) or scissors (2)
+$playerBMove = $throw[rand(0, 2)]; // rock (0) paper (1) or scissors (2)
 
 if ($playerAMove == $playerBMove) {
     $winner = ("it's a tie");
