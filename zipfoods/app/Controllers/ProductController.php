8e23980f455476e08d40879f1f7735d75fc93564
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-11-05 18:45:19 -0500

    commiting Week 9 homework zipfoods About app redux

---
+++ zipfoods/app/Controllers/ProductController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return 'check out all of our cool products';
+    }
+}
