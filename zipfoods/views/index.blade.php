aaf226d682dac6efaefa50d6917883ba2215cffe
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-11-11 14:29:50 -0500

    week 10, added logos, bootstrap, removed app.css

--- zipfoods/views/index.blade.php
+++ zipfoods/views/index.blade.php
@@ -1,13 +1,18 @@
 @extends('templates.master')
 
+@section('head')
+<link href='/css/index.css' rel='stylesheet'>
+@endsection
+
 @section('title')
-    {{ $welcome }}
-@stop
+{{ $welcome }}
+@endsection
 
 @section('content')
-    
-    <h2>{{ $welcome }}</h2>
 
-    <p>Hello and welcome... This is the boilerplate splash page for my application built with  <a href='https://github.com/susanBuck/e2framework'>e2framework</a>.</p>
+<h2>{{ $welcome }}</h2>
+
+<p>Hello and welcome... This is the boilerplate splash page for my application built with <a
+        href='https://github.com/susanBuck/e2framework'>e2framework</a>.</p>
 
 @endsection
