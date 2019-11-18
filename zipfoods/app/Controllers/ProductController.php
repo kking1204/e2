e1799d487555b577b9ddd84258c8b2a9e3542ba3
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-11-18 15:23:16 -0500

    updated Susan solution for redirect

--- zipfoods/app/Controllers/ProductController.php
+++ zipfoods/app/Controllers/ProductController.php
@@ -25,14 +25,18 @@ class ProductController extends Controller
     {
         $id = $this->app->param('id');
         
+        if (is_null($id)) {
+            return $this->app->redirect('/products');
+        }
+        
         $product = $this->products->getById($id);
 
         if (is_null($product)) {
             return $this->app->view('products.missing', ['id' => $id]);
-        } 
+        }
             
-            return $this->app->view('products.show', [
+        return $this->app->view('products.show', [
             'product' => $product
-        ]);       
+        ]);
     }
 }
