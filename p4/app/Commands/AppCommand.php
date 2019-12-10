64d6ca9ff861548b9d227f80d055fd85709ec8dc
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-12-09 19:12:12 -0500

    made some App commands, installed Faker

--- p4/app/Commands/AppCommand.php
+++ p4/app/Commands/AppCommand.php
@@ -8,4 +8,45 @@ class AppCommand extends Command
     {
         dump('It works! You invoked your first command.');
     }
+
+    public function migrate()
+    {
+        $this->app->db()->createTable('results', [
+            'name' => 'varchar(255)',
+            'content' => 'varchar(255)',
+        ]);
+        dump('Migration complete; check the database for your new table.');
+    }
+
+    public function seed()
+    {
+        
+    # Instantiate a new instance of the Faker\Factory class
+    $faker = \Faker\Factory::create();
+
+    # Use a loop to create 10 things
+    for ($i = 0; $i < 10; $i++) {
+
+    # Set up a thing
+    $bs = [
+        'name' => $faker->name,
+        'content' => $faker->sentences(3, true),
+    ];
+
+    # Insert the thing
+    $this->app->db()->insert('results', $bs);
 }
+        dump('Seeds complete!');
+    }
+
+    public function boom()
+    {
+        $this->migrate();
+        $this->seed();
+        dump("Boom! Now get to work.");
+    }
+}
+
+
+
+ 
