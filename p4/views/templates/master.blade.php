382858a2bbf6c0c27859b59b35eab6cbd60c5984
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-12-10 13:17:27 -0500

    added Booststrap style

--- p4/views/templates/master.blade.php
+++ p4/views/templates/master.blade.php
@@ -7,7 +7,7 @@
     <meta charset='utf-8'>
 
     <link rel='shortcut icon' href='/favicon.ico'>
-
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href='/css/app.css' rel='stylesheet'>
 
     @yield('head')
