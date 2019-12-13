469e55a57d12afc2fae6fbf0406d2f69673f7a01
Kenyon King @ Harvard DCE <kenyon_king@harvard.edu>
2019-12-13 16:09:44 -0500

    styles
    shamefully ripped off from the instructor

--- p4/views/index.blade.php
+++ p4/views/index.blade.php
@@ -21,31 +21,17 @@
 	<form method="POST" action="save-new-result">
         <p>Choose one:</p>
         <div class="radio-group">
-            <input type="radio" value="rock" id="rock" name="throw">
-            <label for="rock"> Rock</label>		
-            <input type="radio" value="paper" id="paper" name="throw">
-            <label for="paper"> Paper</label>
-            <input type="radio" value="scissors" id="scissors" name="throw">
-            <label for="scissors"> Scissors</label>	
-            <button type="submit">----Throw!-----</button>		
+            <label for='rock'><input type='radio' name='throw' value='rock' id='rock'> Rock</label>
+            <label for='paper'><input type='radio' name='throw' value='paper' id='paper'> Paper</label>
+            <label for='scissors'><input type='radio' name='throw' value='scissors' id='scissors'> Scissors</label>
+            <button type='submit' class='btn btn-info'>--Throw--</button>
         </div>
 	</form>
 
 {{-- //if there's a winner, display the results --}}
 
 
-{{-- @if('winner')
-    
-    <p>You chose {{ $result['userMove']}} </p>
-
-    <p>Computer chose {{ $result['computerMove']}} </p>
-
-    <p>{{ $result['winner']}}</p>
-
-    <a href = "/results"> &larr;All results</a>
-
-    @endif
-    ========== --}}
+{{--  --}}
 
     <div>
         <a href='/results'>See results &rarr;</a>
